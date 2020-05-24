e8f37325615ffb3dc7c7eb80348d533c89580513
McCrearyD <mccreary@dyllan.ai>
2020-05-23 22:32:07 -0700

    Describe second method

--- graph_nn.py
+++ graph_nn.py
@@ -1,8 +1,9 @@
-
+import numpy as np
 
 class GraphNN:
 
-    def __init__(self, in_neurons, neurons, out_neurons):
+    def __init__(self, in_neurons, neurons, out_neurons, sparsity_value=0.5, force_input_connections=False, \
+                 force_output_connections=False):
         """
         Similar to GraphFFNN, however this class isn't forced to follow a Feed-Forward structure.
         """
@@ -14,7 +15,7 @@ class GraphNN:
         self._out_neurons = out_neurons
         self._neurons = neurons
 
-        self.graph_weights = np.zeros((self.neurons, self.neurons))
+        self.graph_weights = np.zeros((self._neurons, self._neurons))
 
         """
         Weight initialization methods:
@@ -22,10 +23,14 @@ class GraphNN:
         1. Scaling Sparsity:
             One way of initializing weights is to provide a sparsity value. The higher the sparsity vaulue, the lower the chance of allowing a synapse at some position. If the sparsity value is 1, the weights will all be 0s. If the sparsity value is 0, then none of the weights will be 0, therefore 0 sparsity. 0.5 means that there is a 50% chance that a given synaptic value (or weight) is != 0.
 
-        2. Scaling Sparsity + Forced Connections:
-            This method is basically the same as the scaling sparsity method, however it can force input/output neurons to have connections. For example, a feed forward neural network 
+        2. Scaling Sparsity + Forced Connections (Inward/Outward):
+            This method is basically the same as the scaling sparsity method, however it can force input and/or output neurons to have connections. For example, a feed forward neural network forces connections for input neurons to be fully connected to every neuron in the following layer. In FFNNs, the output neurons don't have forced outward connections, however they do have forced INWARD connections, meaning each output neuron is inwardly connected to every neuron in the previous layer.
         """
 
+        if force_input_connections or force_output_connections:
+            raise NotImplementedError('TODO')
+
 if __name__ == '__main__':
-    nn = GraphNN(1, 5, 1)
+    nn = GraphNN(1, 5, 1, sparsity_value=0.5)
+        
     print(nn)
