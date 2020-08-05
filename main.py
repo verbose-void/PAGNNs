c9dafd87b8b14d75682bb4077d8e7a84a666d90d
McCrearyD <mccreary@dyllan.ai>
2020-08-05 00:26:11 -0700

    quantify convergence speed w.r.t num_steps

--- main.py
+++ main.py
@@ -6,30 +6,45 @@ from experiments.graph_ffnn import _step
 
 import numpy as np
 
+
+def count_epochs_to_converge(pann, X, T, num_steps, thresh=1e-4, max_epochs=1000):
+    optimizer = torch.optim.Adam(pann.parameters(), lr=0.01)
+    num_epochs = 0
+    loss_value = 1
+    while loss_value > 0.0001:
+        optimizer.zero_grad()
+        with torch.enable_grad():
+            y = pann(X, num_steps=num_steps).unsqueeze(-1)
+            loss = F.mse_loss(y, T)
+
+        loss.backward()
+        optimizer.step()
+        loss_value = loss.item()
+        num_epochs += 1
+
+        if num_epochs >= max_epochs:
+            break
+
+    return num_epochs
+
 if __name__ == '__main__':
     pann = PANN(10, 1, 1, initial_sparsity=0.5)
     print(pann)
     X = torch.tensor(np.random.uniform(low=0, high=10, size=(1, 1)))
     T = torch.tensor(np.random.uniform(low=0, high=10, size=(1, 1)), dtype=torch.float)
 
-    print(pann.structure_adj_matrix.weight)
+    num_runs = 50
+    steps = (2, 3, 4, 5, 6)
+    averages = []
+    for num in steps:
+        print('testing %i...' % num)
+        total_epochs = 0
+        for _ in range(num_runs):
+            epochs = count_epochs_to_converge(pann, X, T, num)
+            total_epochs += epochs
+        avg = total_epochs / num_runs
+        averages.append(avg)
+        print('num_steps=%i average epochs to converge: %.2f' % (num, avg))
 
-    optimizer = torch.optim.Adam(pann.parameters(), lr=0.01)
-    for epoch in range(500):
-        optimizer.zero_grad()
-        with torch.enable_grad():
-            y = pann(X, num_steps=3).unsqueeze(-1)
-            loss = F.mse_loss(y, T)
+    print(dict(zip(steps, averages)))
 
-        # print(loss.item(), T[0].item(), y[0].item())
-        loss.backward()
-        optimizer.step()
-        # print(pann.structure_adj_matrix.weight.grad)
-
-    print(loss.item(), T[0].item(), y[0].item())
-    print(pann.structure_adj_matrix.weight)
-
-    # state = np.random.uniform(low=0, high=10, size=(10, 10))
-    # print(_step(X, state))
-    # print('\n')
-    # print(X.T.dot(state).T)
