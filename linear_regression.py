32e55d6630d66901e5c1fe8a131818473971b7ee
McCrearyD <mccreary@dyllan.ai>
2020-08-05 12:49:21 -0700

    show off PANNs for linear problems

---
+++ linear_regression.py
@@ -0,0 +1,122 @@
+import torch
+import torch.nn.functional as F
+from torch.utils.data import TensorDataset, DataLoader
+
+from PANN.pann import PANN
+from experiments.graph_ffnn import _step
+
+import numpy as np
+
+from math import isnan
+
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+    seed = None # 666
+    if seed is not None:
+        torch.manual_seed(seed)
+
+    pann = PANN(10, 1, 1, initial_sparsity=0.5)
+    print(pann)
+
+    linear_model = torch.nn.Linear(1, 1)
+    print(linear_model)
+
+    # load data into torch tensors
+    train_df = pd.read_csv('datasets/linear_regression/train.csv')
+    train_df = train_df.dropna()
+    test_df = pd.read_csv('datasets/linear_regression/test.csv')
+    test_df = test_df.dropna()
+    train_x, train_y = torch.tensor(train_df['x'].to_numpy()), torch.tensor(train_df['y'].to_numpy())
+    test_x, test_y = torch.tensor(test_df['x'].to_numpy()), torch.tensor(test_df['y'].to_numpy())
+    # print('train shapes', train_x.shape, train_y.shape)
+    # print('test shapes', test_x.shape, test_y.shape)
+
+    # create data loaders
+    batch_size = 1
+    train_dl = DataLoader(TensorDataset(train_x, train_y), batch_size=batch_size)
+    test_dl = DataLoader(TensorDataset(test_x, test_y), batch_size=batch_size)
+
+    optimizer = torch.optim.Adam(pann.parameters(), lr=0.001)
+    baseline_optimizer = torch.optim.Adam(linear_model.parameters(), lr=0.0002)
+    num_steps = 5
+
+    pann_history = {'train_loss': [], 'test_loss': []}
+    baseline_history = {'train_loss': [], 'test_loss': []}
+    
+    for epoch in range(10):
+        with torch.enable_grad():
+            pann_total_loss = 0
+            baseline_total_loss = 0
+
+            for x, t in train_dl:
+                optimizer.zero_grad()
+
+                x = x.float().unsqueeze(-1)
+                t = t.float().unsqueeze(-1)
+
+                y = pann(x, num_steps=num_steps).unsqueeze(-1)
+                baseline_y = linear_model(x)
+
+                loss = F.mse_loss(y, t)
+                baseline_loss = F.mse_loss(baseline_y, t)
+                pann_total_loss += loss.item()
+                baseline_total_loss += baseline_loss.item()
+
+                loss.backward()
+                optimizer.step()
+
+                baseline_loss.backward()
+                baseline_optimizer.step()
+
+            pann_avg_loss = pann_total_loss / len(train_dl)
+            baseline_avg_loss = baseline_total_loss / len(train_dl)
+            print('[PANN] average loss for epoch %i: %f' % (epoch, pann_avg_loss))
+            print('[BASELINE] average loss for epoch %i: %f' % (epoch, baseline_avg_loss))
+
+            pann_history['train_loss'].append(pann_avg_loss)
+            baseline_history['train_loss'].append(baseline_avg_loss)
+
+        with torch.no_grad():
+            total_loss = 0
+            baseline_total_loss = 0
+            for x, t in test_dl:
+                x = x.float().unsqueeze(-1)
+                t = t.float().unsqueeze(-1)
+
+                y = pann(x, num_steps=num_steps).unsqueeze(-1)
+                baseline_y = linear_model(x)
+
+                loss = F.mse_loss(y, t)
+                total_loss += loss.item()
+
+                baseline_loss = F.mse_loss(baseline_y, t)
+                baseline_total_loss += baseline_loss.item()
+
+            pann_avg_loss = total_loss / len(test_dl)
+            baseline_avg_loss = baseline_total_loss / len(test_dl)
+
+            print('[PANN] testing loss:', pann_avg_loss)
+            print('[BASELINE] testing loss:', baseline_avg_loss)
+
+            pann_history['test_loss'].append(pann_avg_loss)
+            baseline_history['test_loss'].append(baseline_avg_loss)
+
+
+    # print(pann_history)
+    # print(baseline_history)
+
+    plt.plot(pann_history['train_loss'], label='PANN')
+    plt.plot(baseline_history['train_loss'], label='Baseline')
+    plt.legend()
+    plt.title('train loss')
+    plt.show()
+
+    plt.plot(pann_history['test_loss'], label='PANN')
+    plt.plot(baseline_history['test_loss'], label='Baseline')
+    plt.legend()
+    plt.title('test loss')
+    plt.show()
