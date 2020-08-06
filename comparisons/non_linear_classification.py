48ece406d843a82131cf60b40bd42de6d0b2b76a
McCrearyD <mccreary@dyllan.ai>
2020-08-06 14:26:24 -0700

    use networkx complete_graph generator

--- comparisons/non_linear_classification.py
+++ comparisons/non_linear_classification.py
@@ -69,7 +69,7 @@ if __name__ == '__main__':
     print('number of classes:', C)
 
     # create models
-    pagnn = PAGNN(D + C + 5, D, C, initial_sparsity=0.2)
+    pagnn = PAGNN(D + C + 5, D, C, graph_generator=nx.generators.classic.complete_graph)
     print(pagnn)
     linear_model = FFNN(D, 25, C) # torch.nn.Linear(D, C)
     print(linear_model)
