e387bb146fb6a9bff416e7ccd8e38b0722552d1e
McCrearyD <mccreary@dyllan.ai>
2020-11-01 14:23:29 -0800

    added activation function

--- pagnn/pagnn.py
+++ pagnn/pagnn.py
@@ -6,7 +6,7 @@ def _pagnn_op(state, weight):
 
 
 class PAGNNLayer(torch.nn.Module):
-    def __init__(self, input_neurons, output_neurons, extra_neurons, steps=1, sparsity=0, retain_state=True):
+    def __init__(self, input_neurons, output_neurons, extra_neurons, steps=1, sparsity=0, retain_state=True, activation=None):
         super(PAGNNLayer, self).__init__()
 
         assert input_neurons > 0 and output_neurons > 0 and extra_neurons >= 0 and sparsity >= 0
@@ -20,16 +20,21 @@ class PAGNNLayer(torch.nn.Module):
         self._sparsity = sparsity
         self._retain_state = retain_state
         self._steps = steps
+
+        if activation is None:
+            activation = lambda x: x
         
         self.weight = torch.nn.Parameter(torch.zeros((self._total_neurons, self._total_neurons)))
         self.bias = torch.nn.Parameter(torch.zeros(self._total_neurons))
+        self.activation = activation
         self.state = torch.zeros(self._total_neurons)
         torch.nn.init.kaiming_uniform_(self.weight, mode='fan_in')
 
     def step(self, n=1):
-        for _ in range(n):
-            self.state = _pagnn_op(self.state, self.weight)
-            self.state += self.bias
+        for step in range(n):
+            self.state = _pagnn_op(self.state, self.weight) + self.bias
+            if step < n-1:
+                self.state = self.activation(self.state)
 
     def forward(self, x):
         self.load_input_neurons(x)
