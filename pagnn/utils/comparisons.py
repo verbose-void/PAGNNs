cb54f91cc38612b267745ce94c695e57776b8505
McCrearyD <mccreary@dyllan.ai>
2020-11-01 21:57:16 -0800

    flat dim for images

--- pagnn/utils/comparisons.py
+++ pagnn/utils/comparisons.py
@@ -62,7 +62,7 @@ def get_dataloaders(data_tensors, batch_size):
     return train_dl, test_dl
 
 
-def compare(model_dicts, train_dl, test_dl, epochs, criterion, use_tqdm=True, test_accuracy=False, device=torch.device('cpu')):
+def compare(model_dicts, train_dl, test_dl, epochs, criterion, use_tqdm=True, test_accuracy=False, device=torch.device('cpu'), flat_dim=None):
     try:
         for model_dict in model_dicts:
             model_dict['train_history'] = []
@@ -86,6 +86,9 @@ def compare(model_dicts, train_dl, test_dl, epochs, criterion, use_tqdm=True, te
                         iterator = tqdm(iterator, desc='[train] %s' % (model_name), total=len(iterator))
 
                     for x, t in iterator:
+                        if flat_dim is not None:
+                            x = x.flatten(flat_dim)
+
                         x = x.to(device) #.unsqueeze(0)
                         t = t.to(device) #.unsqueeze(0)
 
@@ -117,6 +120,9 @@ def compare(model_dicts, train_dl, test_dl, epochs, criterion, use_tqdm=True, te
                         iterator = tqdm(iterator, desc='[test] %s' % (model_name), total=len(iterator))
 
                     for x, t in iterator:
+                        if flat_dim is not None:
+                            x = x.flatten(flat_dim)
+
                         x = x.to(device)
                         t = t.to(device)
 
