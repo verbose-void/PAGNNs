38ed900ccaccb165c1ac41175a5a0def0ee77958
McCrearyD <mccreary@dyllan.ai>
2020-11-01 21:12:26 -0800

    compare Linear layer & less PAGNNs

--- examples/mushroom_classification.py
+++ examples/mushroom_classification.py
@@ -57,23 +57,31 @@ if __name__ == '__main__':
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
     model_dicts = []
+
+    linear_lr = 0.01
+    linear_model = torch.nn.Linear(D, C)
+    linear_model.to(device)
+    linear = {
+        'name': 'Linear(%i, %i, #p=%i)' % (D, C, count_params(linear_model)),
+        'model': linear_model,
+        'optimizer': torch.optim.Adam(linear_model.parameters(), lr=linear_lr),
+    }
+
+    model_dicts.append(linear)
+
     configs = [
-        {'initial_sparsity': 0, 'num_steps': 5},
-        # {'initial_sparsity': 0.1, 'num_steps': 5},
-        # {'initial_sparsity': 0.5, 'num_steps': 5},
-        # {'initial_sparsity': 0.8, 'num_steps': 5},
-        # {'initial_sparsity': 0.9, 'num_steps': 5},
-        # {'initial_sparsity': 0.98, 'num_steps': 5},
+        {'num_steps': 1},
+        {'num_steps': 3},
     ]
 
     for config in configs:
         pagnn_lr = 0.001
-        extra_neurons = 5
+        extra_neurons = 0
         n = D + C + extra_neurons
         pagnn_model = PAGNNLayer(D, C, extra_neurons, steps=config['num_steps'], retain_state=False) # graph_generator=nx.generators.classic.complete_graph)
         pagnn_model.to(device)
         pagnn = {
-            'name': 'PAGNNLayer(neurons=%i, total_params=%i)' % (n, count_params(pagnn_model)),
+            'name': 'PAGNNLayer(n=%i, #p=%i, steps=%i)' % (n, count_params(pagnn_model), config['num_steps']),
             'model': pagnn_model,
             # 'num_steps': config['num_steps'],
             'optimizer': torch.optim.Adam(pagnn_model.parameters(), lr=pagnn_lr),
@@ -85,7 +93,7 @@ if __name__ == '__main__':
     ffnn_model = FFNN(D, D, C)
     ffnn_model.to(device)
     ffnn = {
-        'name': 'FFNN(%i, %i, %i, total_params=%i)' % (D, D, C, count_params(ffnn_model)),
+        'name': 'FFNN(%i, %i, %i, #p=%i)' % (D, D, C, count_params(ffnn_model)),
         'model': ffnn_model,
         'optimizer': torch.optim.Adam(ffnn_model.parameters(), lr=ffnn_lr),
     }
