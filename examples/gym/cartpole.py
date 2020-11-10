959e7431d7b42b0a846b0cd8ca674a4d4c49f1a0
McCrearyD <mccreary@dyllan.ai>
2020-11-09 20:01:51 -0800

    run generations of populations

--- examples/gym/cartpole.py
+++ examples/gym/cartpole.py
@@ -1,4 +1,5 @@
 import gym
+import numpy as np
 import torch
 from pagnn.pagnn import PAGNNLayer
 
@@ -33,7 +34,18 @@ def play_episode(network, episodes=20, max_steps=100, render=False, verbose=Fals
     return scores
 
 
+def run(generations=10, population_size=10):
+    genomes = [PAGNNLayer(4, 2, 4) for _ in range(population_size)]
+    best_genome = None
+
+    for generation in range(generations):
+        avg_scores_per_genome  = []
+        for i, genome in enumerate(genomes):
+            scores_per_episode = play_episode(genome)
+            avg_scores_per_genome.append(np.mean(scores_per_episode).item())
+
+        print(avg_scores_per_genome)
+    
+
 if __name__ == '__main__':
-    pagnn = PAGNNLayer(4, 2, 4)
-    scores_per_episode = play_episode(pagnn)
-    print(scores_per_episode)
+    run()
