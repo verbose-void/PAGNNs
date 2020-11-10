73459d49849dda08d5e5d518c2e3b469ab29add8
McCrearyD <mccreary@dyllan.ai>
2020-11-09 22:31:37 -0800

    use 0 extra neurons, still get optimal policy

--- examples/gym/mountaincar.py
+++ examples/gym/mountaincar.py
@@ -3,4 +3,4 @@ from examples.gym.utils import run, deterministic
 
 if __name__ == '__main__':
     deterministic()
-    run('MountainCar-v0', generations=10, population_size=100, extra_neurons=3, best_replay=True, search_type='evolutionary', retain_state=False)
+    run('MountainCar-v0', generations=10, population_size=100, extra_neurons=0, best_replay=True, search_type='evolutionary', retain_state=False)
