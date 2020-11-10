e92147a35774614d9974e07e1ef296675187cec4
McCrearyD <mccreary@dyllan.ai>
2020-11-09 22:09:23 -0800

    params

--- examples/gym/cartpole.py
+++ examples/gym/cartpole.py
@@ -2,4 +2,4 @@ from examples.gym.utils import run
 
 
 if __name__ == '__main__':
-    run('CartPole-v0', best_replay=True, search_type='evolutionary')
+    run('CartPole-v0', generations=10, population_size=100, extra_neurons=0, best_replay=True, search_type='evolutionary')
