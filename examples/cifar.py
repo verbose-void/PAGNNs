f143baff0b06469b36c9f6653cb92a320e3376f2
McCrearyD <mccreary@dyllan.ai>
2020-11-12 23:24:34 +0000

    do 350 epochs

--- examples/cifar.py
+++ examples/cifar.py
@@ -138,7 +138,7 @@ def test(epoch):
         best_acc = acc
 
 
-for epoch in range(start_epoch, start_epoch+200):
+for epoch in range(start_epoch, start_epoch+350):
     train(epoch)
     test(epoch)
     lr_scheduler.step()
