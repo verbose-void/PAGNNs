a2378fe57fae44862e454ea085ecc853f973a551
McCrearyD <mccreary@dyllan.ai>
2020-11-13 14:25:10 -0800

    building FFNN importer & tests

---
+++ pagnn/utils/importer.py
@@ -0,0 +1,13 @@
+"""This file is responsible for importing various networks into the PAGNN architecture"""
+
+import torch
+from pagnn import PAGNNLayer
+
+
+
+def import_ffnn(ffnn):
+    """Ingest a FFNN into the PAGNN architecture"""
+
+    print(ffnn)
+
+    return None
---
+++ tests/test_importer.py
@@ -0,0 +1,45 @@
+import torch
+import numpy as np
+
+from torch.nn import Sequential, Linear, ReLU
+
+from pagnn.utils.importer import import_ffnn
+
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+np.random.seed(666)
+torch.manual_seed(666)
+
+
+def test_import_ffnn():
+    X = torch.rand((50, 1))
+    T = torch.rand((50, 1))
+
+    net1 = Sequential(
+        Linear(1, 5),
+        ReLU(),
+        Linear(5, 5,),
+        ReLU(),
+        Linear(5, 1)
+    )
+
+    print('FFNN:')
+    print(net1)
+
+    Y = net1(X)
+
+    print('FFNN output:')
+    print(Y)
+
+    net1_pagnn = import_ffnn(net1)
+
+    print('imported PAGNN:')
+    print(net1_pagnn)
+
+    imported_Y = net1_pagnn(X)
+
+    print('imported PAGNN output:')
+    print(imported_Y)
+    
+    assert False
