5d7448838b71842914d19236deabc7a034c7d3c6
McCrearyD <mccreary@dyllan.ai>
2020-11-14 02:21:22 +0000

    test resnet18 & p_resnet18 equivalence

--- tests/test_importer.py
+++ tests/test_importer.py
@@ -5,6 +5,8 @@ from torch.nn import Sequential, Linear, ReLU
 import torch.nn.functional as F
 
 from pagnn.pagnn import import_ffnn, count_neurons
+from pagnn.p_resnet import p_resnet18
+from torchvision.models import resnet18
 
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
@@ -89,3 +91,16 @@ def test_import_3layer():
     )
 
     assert_import_equivalence(net, X)
+
+def test_import_resnet():
+    res = resnet18(pretrained=True)
+    pres = p_resnet18()
+    pres.load_state_dict(res.state_dict())
+
+    X = torch.rand((5, 3, 224, 224))
+    
+    pres_Y = pres(X)
+    res_Y = res(X)
+
+    error = F.mse_loss(pres_Y, res_Y)
+    assert error < 1e-3, 'All elements of P-ResNet Y must equal ResNet Y. MSError: %f' % error
